6ae27463de43e43987041c9b08fa4b99b6c75957
Sarang <sarangkrishna04@gmail.com>
2019-01-31 10:28:11 +0530

    Greet new user and alert other users

--- server/server.js
+++ server/server.js
@@ -18,9 +18,26 @@ io.on('connection', (socket)=>{
   //   createdAt: 1234
   // });
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message)=>{
     console.log('createMessage', message);
-    io.emit('newMessage', {               //EMITS MESSAGEGES TO MULTIPLE USERS
+    // io.emit('newMessage', {               //EMITS MESSAGEGES TO MULTIPLE USERS
+    //   from:message.from,
+    //   text:message.text,
+    //   createdAt:new Date().getTime()
+    // });
+    socket.broadcast.emit('newMessage', {   //EMITS MESAGES TO OTHERS AND NOT FOR ITSELF
       from:message.from,
       text:message.text,
       createdAt:new Date().getTime()
