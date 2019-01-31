9f776a2ae13f19386ac71b18d688bf317fce7de8
Sarang <sarangkrishna04@gmail.com>
2019-01-31 17:39:54 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -36,14 +36,14 @@ socket.on('newLocationMessage', function(message) {
   jQuery('#messages').append(li);
 });
 
-
+var messageTextbox = jQuery('[name=message]');
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function() {
-
+    messageTextbox.val('');
   });
 });
 
@@ -53,13 +53,16 @@ locationButton.on('click', function() {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported on your browser');
   }
+  locationButton.attr('disabled', 'disabled').text('Sending location..');
   navigator.geolocation.getCurrentPosition(function (position) {
     // console.log(position);
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     })
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   });
 });
